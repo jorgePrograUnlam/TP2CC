f44c14efd43e747ecc0f15a029967c8249a6ebe7
jorge <jamc4626@gmail.com>
2017-09-16 23:37:54 -0300

    agregando caso de prueba para BinomioDeNewton

---
+++ src/test/TestBinomioDeNewton.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBinomioDeNewton {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void probarMetedoDeBinomio() {
+		Assert.fail("Not yet implemented");
+	}
+
+}
