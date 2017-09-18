b1ef152ebad106ddc695a1659189bbc4026d7758
Juan Olivadese <juanolivadese@hotmail.com>
2017-09-18 20:45:49 -0300

    Agregado get Coeficientes con prog.dinamica recursivo y no recursivo

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,9 +11,10 @@ public class Main {
 
 
 	public static void main(String[] args) {
-
-		probarReslucionDePolinomio();
-		probarBinomioDeNewton();
+		
+		//probarReslucionDePolinomio();
+		//probarBinomioDeNewton();
+		
 	}
 
 	public static void probarBinomioDeNewton() {
--- src/polinomio/BinomioDeNewton.java
+++ src/polinomio/BinomioDeNewton.java
@@ -1,4 +1,7 @@
 package polinomio;
+
+import java.util.Arrays;
+
 public class BinomioDeNewton {
 
 	// Binomio de la forma (ax + b)^n
@@ -20,7 +23,54 @@ public class BinomioDeNewton {
 
 		return r1*r2*r3;
 	}
-
+	 
+	// 0(n*k)
+	public double getCoeficienteKDinamicaNoRec(int k) {
+		if(k<=0 || k > n+1  )
+			return 0;
+		
+		double matrizCoeficientes[][] = new double[n+1][k];
+		
+		for(int i=0;i < n+1;i++){
+			matrizCoeficientes[i][0] = 1;
+		}
+		
+		for(int j=0;j < k && j<n+1;j++){
+			matrizCoeficientes[j][j] = 1;
+		}
+		
+		for(int i=2;i < n+1 ;i++){
+			for(int j=1;j < i && j<k ;j++) {
+				matrizCoeficientes[i][j] = matrizCoeficientes[i-1][j-1]+matrizCoeficientes[i-1][j];
+			}
+		}
+		
+		return matrizCoeficientes[n][k-1];
+	}
+	
+	
+	// O(n)?????????
+	public double getCoeficienteKDinamicaRec(int k) {
+		int i,j;
+			
+		if(k<=0 || k > n+1)
+			return 0;
+		i=n-1;
+		j=k-1;
+			
+		double matrizCoeficientes[][] = new double[n+1][k];
+			
+		return matrizCoeficientes[n][k-1] = calculo(matrizCoeficientes,i,j);
+			
+	}
+	
+	public double calculo(double mat[][],int i, int j) {
+		if(i==j || j==0) 
+			return mat[i][j] = 1;
+		
+		else return calculo(mat, i-1, j-1) + calculo(mat, i-1, j);
+	}
+	
 	// no recursivo
 	// O(N^3)
 	public Polinomio getFormaPolinomica() {
@@ -37,5 +87,18 @@ public class BinomioDeNewton {
 		return new Polinomio(n, coeficientes);
 
 	}
+	
+	public void printMatrix(double mat[][],int n, int k) {
+		
+		for(int i=0 ; i<n;i++) {
+			for(int j=0 ; j<k; j++) {
+				System.out.print(mat[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+	
 
 }
